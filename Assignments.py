e5db0989de2e0364a9ae45320295c4a20a239622
NavarraB <navarrabueng@gmail.com>
2020-07-07 13:33:16 +0300

    Assignment - 2 (if-statements) has been done.

--- Assignments.py
+++ Assignments.py
@@ -11,3 +11,28 @@ if passname.lower() == 'navarra':
 else:
     print('Hello {}! See you later.'.format(passname.capitalize()))
 
+
+# In[ ]:
+
+
+# Assignment - 2 (if-statements)
+cigar = input('Are you a cigarette addict older than 75 years old? ')
+disease = input('Do you have a severe chronic disease? ')
+immune = input('Is your immune system too weak? ')
+if cigar.lower() in ('yes', 'yep', 'right', 'yea', 'yeap', 'unfortunately'):
+    cigar = True
+else:
+    cigar = False
+if disease.lower() in ('yes', 'yep', 'right', 'yea', 'yeap', 'unfortunately'):
+    disease = True
+else:
+    disease = False
+if immune.lower() in ('yes', 'yep', 'right', 'yea', 'yeap', 'unfortunately'):
+    immune = True
+else:
+    immune = False
+if cigar or disease or immune:
+    print('You are in risky group')
+else:
+    print('You are not in risky group')
+
