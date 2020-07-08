86ef45374aca778978dca8fd093a9b7da9c971ef
NavarraB <navarrabueng@gmail.com>
2020-07-08 14:31:45 +0300

    Assignment - 3 (Is it Armstrong Number?) has been done.

--- Assignments.py
+++ Assignments.py
@@ -36,3 +36,32 @@ if cigar or disease or immune:
 else:
     print('You are not in risky group')
 
+
+# In[ ]:
+
+
+# Assignment - 3 (Is it Armstrong Number?)
+given_num = input('Please enter a number: ')
+given_numset = set(given_num)
+forbid_set1 = set('-')
+forbid_set2 = set(',.')
+while bool(given_numset & forbid_set1) or bool(given_numset & forbid_set2) or given_num.isnumeric() != True: 
+    if bool(given_numset & forbid_set1):
+        given_num = input('Please enter a positive number: ')
+        given_numset = set(given_num)
+    elif bool(given_numset & forbid_set2):
+        given_num = input('Please enter a integer number: ')
+        given_numset = set(given_num)
+    elif given_num.isnumeric() != True:
+        given_num = input('Do not use any entries other than numeric values. Please enter a number: ')
+        given_numset = set(given_num)
+given_arm_num1 = int(given_num)
+given_arm_num2 = 0
+for x in given_num:
+    int(x) ** len(given_num)
+    given_arm_num2 += (int(x) ** len(given_num))
+if given_arm_num1 == given_arm_num2:
+    print('{} is an Armstrong number'.format(given_arm_num1))
+else:
+    print('{} is not an Armstrong number'.format(given_arm_num1))
+
